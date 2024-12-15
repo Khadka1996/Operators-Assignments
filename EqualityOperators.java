519c6bfb74dc248904a79f37f68c3d9c7a9ffa79
Manish Khadka <mnzkhadka1996@gmail.com>
2024-12-16 05:18:32 +0545

    Added new features or changes

--- EqualityOperators.java
+++ EqualityOperators.java
@@ -1,5 +1,14 @@
 public class EqualityOperators {
     public static void main(String[] args) {
+
+       Thread T1=new Thread();
+       Thread T2=new Thread();
+       Thread T3 = T1;
+       System.out.println(T1==T2);  // false because both objects are different instances of Thread class.
+       System.out.println(T1==T3);
+
+       System.out.println("******************************");
+       
         System.out.println(10==20);
         System.out.println('c'=='c');
         System.out.println('a'=='b');
